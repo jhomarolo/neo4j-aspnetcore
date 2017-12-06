d57123d1edcaf103a0e1bd5afa7012b8332b48ba
Jhonathan Soares <jhonathanmarolo@hotmail.com>
2017-12-06 15:23:06 -0200

    Início do projeto

---
+++ .gitignore/Neo4jAspCore/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Neo4j.Driver.V1;
+
+namespace Neo4jAspCore
+{
+    class Program
+    {
+        public static void Main()
+        {
+            using (var greeter = new HelloWorldExample("bolt://localhost:7687", "neo4j", "Mudar123"))
+            {
+                greeter.PrintGreeting("hello, world from DevWeek");
+                greeter.DeleteDatabase();
+            }
+        }
+    }
+}
+
+
+
+public class HelloWorldExample : IDisposable
+{
+    private readonly IDriver _driver;
+
+    public HelloWorldExample(string uri, string user, string password)
+    {
+        _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+    }
+
+    public void PrintGreeting(string message)
+    {
+        using (var session = _driver.Session())
+        {
+            var greeting = session.WriteTransaction(tx =>
+            {
+                var result = tx.Run("CREATE (a:Greeting) " +
+                                    "SET a.message = $message " +
+                                    "RETURN a.message + ', from node ' + id(a)",
+                    new { message });
+                return result.Single()[0].As<string>();
+            });
+            Console.WriteLine(greeting);
+        }
+    }
+
+    public void DeleteDatabase()
+    {
+
+        using (var session = _driver.Session())
+        {
+            var greeting = session.WriteTransaction(tx =>
+            {
+                var result = tx.Run("MATCH (n) DETACH DELETE n");
+                return "Database deleted";
+            });
+            Console.WriteLine(greeting);
+        }
+    }
+
+    public void Dispose()
+    {
+        _driver?.Dispose();
+    }
+}
